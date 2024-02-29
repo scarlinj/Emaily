b1a6b9e4a31d693f6debc022949b77bc6ee7969d
Stephen Carlin <scarlinj@gmail.com>
2024-02-29 15:42:12 -0800

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // send request using "app" - app.get (get info), app.post (send info), put (update all properties of something), delete )(delete something), patch (update one or two properties)
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 });
 
 // use environment (env) variable to assign port, since Heroku will assign the port and it cannot be predicted.  If in development environment, or if no PORT assigned, will use default PORT 5000
